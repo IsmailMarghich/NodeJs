1cfc37b0e9b89cf4a4aa6eee3d5e6b9031b7c31f
IsmailMarghich <ismailcodes@gmail.com>
2022-05-31 21:01:34 +0200

    Learned about mongoDB and started with mongoose, connected application with mongoose to an online mongo database

--- natours/server.js
+++ natours/server.js
@@ -1,4 +1,10 @@
 import app from "./app.js";
+import mongoose from "mongoose";
+import { CONNECTION_STRING } from "./secret.js";
+
+mongoose.connect(CONNECTION_STRING, {}).then((con) => {
+  console.log("connected to database");
+});
 
 app.listen(3000, () => {
   console.log("Server launched");
